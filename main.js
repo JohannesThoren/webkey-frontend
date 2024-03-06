134518a35d63d8ca9ae8d3d706267ddae0696d24
Johannes Thorén <Johannes@lgjt.xyz>
2024-03-06 20:58:53 +0100

    wip

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const socket = io(urlParams.get('ws'));
+
 
 let Global = {
     selectedProfile: ""
@@ -8,16 +6,22 @@ let Global = {
 
 let GlobalProfiles = {}
 
-socket.on('connect', () => {
-    socket.emit('fetch_profiles_data')
-})
 
-socket.on('profiles', (data) => {
-    GlobalProfiles = data["profiles"]
-    SetupControls(GlobalProfiles)
-    UpdateButtons()
-})
 
 window.addEventListener("load", () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const socket = io(urlParams.get('ws'));
+
+    socket.on('connect', () => {
+        socket.emit('fetch_profiles_data')
+    })
+
+    socket.on('profiles', (data) => {
+        GlobalProfiles = data["profiles"]
+        SetupControls(GlobalProfiles)
+        UpdateButtons()
+    })
     CreateButtons(socket)
 })
